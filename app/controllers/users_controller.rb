566cddb6e510962fa593da9c881157221f327aa8
Lukas Camargo <lukas.fialho@gmail.com>
2016-03-29 23:49:45 -0300

    Second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,41 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(params[:user])
+		@user = User.new(user_params)
 		if @user.save
-			redirect_to @user, notice: 'Cadastro criado com sucesso"'
+			redirect_to @user, 
+				notice: 'Cadastro criado com sucesso!'
 
 		else
 			render action: :new
 		end
 	end
 
+	def show
+		user_find
+	end
+
+	def edit
+		user_find
+	end
+
+	def update
+		user_find
+		if @user.update(user_params)
+			redirect_to @user,
+				notice: 'Cadastro atualizado com sucesso!'
+		else
+			render action: :edit
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:email, :full_name, :location, :password, :password_confirmation, :bio)
+	end
+
+	def user_find
+		@user = User.find(params[:id])
+	end
 end
