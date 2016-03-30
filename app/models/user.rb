026e42c3862eebdff53f560303797fc5dd50c9ec
Lukas Camargo <lukas.fialho@gmail.com>
2016-03-29 22:12:27 -0300

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ActiveRecord::Base
+	EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+	validates_presence_of :email, :full_name, :location, :password
+	validates_confirmation_of :password
+	validates_length_of :bio, minimum: 30, allow_blank: false
+	validates_uniqueness_of :email
+
+	# Essa validação pode ser representada da seguinte forma:
+	# validates_format_of :email, with: EMAIL_REGEXP
+	validate do
+		errors.add(:email, :invalid) unless email.match(EMAIL_REGEXP)
+		# validates_format_of :email, with: EMAIL_REGEXP
+	end
+
+
+end
